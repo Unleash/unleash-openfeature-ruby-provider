82aa3eb81c1f7a4f6e0df007a8f4676792061266
Simon Hornby <liquidwicked64@gmail.com>
2026-07-23 15:11:35 +0200

    chore: release 0.1.0

--- lib/unleash/open_feature/provider/version.rb
+++ lib/unleash/open_feature/provider/version.rb
@@ -3,7 +3,7 @@
 module Unleash
   module OpenFeature
     module Provider
-      VERSION = '0.1.0-alpha.2'
+      VERSION = '0.1.0'
       SDK_FLAVOR = 'unleash-openfeature-ruby-provider'
       SDK_FLAVOR_VERSION = VERSION
     end
